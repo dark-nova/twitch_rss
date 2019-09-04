fce7c7e5e119da5a7dfded689be8f2e9df1bcf40
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-09-03 23:54:18 -0700

    Add entry processing for feed

--- generate_feed.py
+++ generate_feed.py
@@ -1,6 +1,10 @@
+import sqlite3
+
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 from selenium import webdriver
+
+import db
 
 
 URL = 'https://twitch.amazon.com/tp/loot'
@@ -14,17 +18,22 @@ def get_as_html():
     browser = webdriver.Firefox(options = options)
     browser.get(URL)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
-    with open('example.html', 'w') as f:
-        f.write(soup.prettify())
+    # with open('example.html', 'w') as f:
+    #     f.write(soup.prettify())
+    return soup
 
 
-def get_all_loot(soup = None):
+def get_all_loot(fg: FeedGenerator, soup = None):
     """Gets all loot from page.
 
     Loot can be categorized as either 'in-game' or 'games'.
 
     Args:
+        fg (FeedGenerator): the feed to add entries
         soup (optional): the soup to use; defaults to None
+
+    Returns:
+        None
 
     """
 
@@ -34,28 +43,63 @@ def get_all_loot(soup = None):
 
     for loot in soup.find_all('div', 'offer-list__content'):
         category = loot.find('h3').text.strip()
-        get_loot(loot, category)
+        get_loot(fg, loot, category)
 
     return
 
 
-def get_loot(loot, category):
+def get_loot(fg: FeedGenerator, loot, category: str):
     """Gets loot for a given `loot` type.
 
     Called by `get_all_loot`.
 
     Args:
+        fg (FeedGenerator): the feed to add entries
         loot: the loot to parse
+        category (str): either 'In-Game Loot and More'
+            or 'Games with Prime'
+
+    Returns:
+        None
 
     """
     for offer in loot.find_all('div', 'offer'):
-        offer_name = offer.find('span').text.strip()
+        entry = fg.add_entry()
+        entry.title(offer.find('span').text.strip())
+        entry.category(
+            category = {
+                'label': category
+                }
+            )
         try:
-            offer_link = offer.find('a')['href']
+            link = offer.find('a')['href']
+            entry.link(href = link)
+            entry.guid(link)
         except TypeError:
-            offer_link = URL
+            entry.link(href = URL)
+            entry.guid(URL)
+
+    return
 
 
 if __name__ == '__main__':
+    fg = FeedGenerator()
+    fg.title('Twitch Prime Games and Loot')
+    fg.author({'name': 'Twitch Prime'})
+    fg.description('Twitch Prime Games and Loot')
+    fg.link(
+        href = URL,
+        rel = 'alternate'
+        )
+    # Change the below URL when self-hosting.
+    fg.link(
+        href = 'https://dark-nova.me/twitchprime.xml',
+        rel = 'self'
+        )
+    # Change the below URL when self-hosting.
+    fg.logo('https://dark-nova.me/twitchprime.png')
+    fg.language('en-US')
+    get_all_loot(fg, get_as_html())
+
+
     #get_as_html()
-    pass
