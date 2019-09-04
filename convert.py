e3b87f425f4e19c4f928dc4bc0a41731ba089c5c
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-09-03 22:41:06 -0700

    Add headless option

--- convert.py
+++ convert.py
@@ -5,12 +5,16 @@ URL = 'https://twitch.amazon.com/tp/loot'
 
 
 def get_as_html():
+    """Gets the true source of the URL."""
     # Choose your own webdriver if desired
-    browser = webdriver.Firefox()
+    options = webdriver.firefox.options.Options()
+    options.headless = True
+    browser = webdriver.Firefox(options = options)
     browser.get(URL)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     with open('example.html', 'w') as f:
         f.write(soup.prettify())
 
 if __name__ == '__main__':
-    get_as_html()
+    #get_as_html()
+    pass
