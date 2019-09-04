8aa33d502532f6d91e26bef3b0f989a2057dd37a
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-09-04 01:44:09 -0700

    Add helper to identify classes in html

---
+++ class_count.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+from collections import defaultdict
+from html.parser import HTMLParser
+
+classes = defaultdict(int)
+
+class ClassCounter(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        for (attr, val) in attrs:
+            if attr == 'class':
+                classes[val] += 1
+
+parser = ClassCounter()
+
+try:
+    with open(sys.argv[1], 'r') as f:
+        parser.feed(f.read())
+    print(json.dumps(classes, indent = 4))
+except Exception as e:
+    print(e)
